d034616f9c7cc4ecbfaad0659e557d1771fad8f1
mayukuno <kunomayu1006@gmail.com>
2020-10-15 21:08:38 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,20 +1,3 @@
-# require 'carrierwave/storage/abstract'
-# require 'carrierwave/storage/file'
-# require 'carrierwave/storage/fog'
-
-# CarrierWave.configure do |config|
-#   config.fog_credentials = {
-#     provider: 'AWS',
-#     aws_access_key_id: ENV['AWS_IAM_ACCESS_KEY_ID'],
-#     aws_secret_access_key: ENV['AWS_IAM_ACCESS_KEY'],
-#     region: ENV['AWS_S3_REGION'],
-#   }
-
-#   config.fog_directory  =  ENV['AWS_S3_BUCKET']
-#   config.asset_host = "https://s3.ap-northeast-1.amazonaws.com/ninefsblog"
-
-# end
-
 
 
 if Rails.env.production?
