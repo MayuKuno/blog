ae4f4b00af46421f2c1438031d058a510e16bb9b
mayukuno <kunomayu1006@gmail.com>
2020-11-26 23:39:34 +0900

    updated

---
+++ app/assets/javascripts/work.js
@@ -0,0 +1,16 @@
+$(function(){
+  $('.work__contents__tab-contents>div').hide();
+  $('.work__contents__tab-contents>div').first().slideDown();
+    $('.work__contents__tabs__tab-buttons span').click(function(){
+      var thisclass=$(this).attr('class');
+      $('#lamp').removeClass().addClass('#lamp').addClass(thisclass);
+      $('.work__contents__tab-contents>div').each(function(){
+        if($(this).hasClass(thisclass)){
+          $(this).fadeIn(800);
+        }
+        else{
+          $(this).hide();
+        }
+      });
+    });
+  });
