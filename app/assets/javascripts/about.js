af7fe4507eb2f8c640584da1856f4a4decf2c1f5
mayukuno <kunomayu1006@gmail.com>
2020-12-15 19:28:48 +0900

    Update about.js

--- app/assets/javascripts/about.js
+++ app/assets/javascripts/about.js
@@ -1,27 +1,27 @@
-// $(function(){
-//   $(window).on('scroll',function (){
-//     let triggerClass = $('.bar');       //トリガーとなるクラス
-//         animateClass = ('about__bottom__skills__container__skill__graph__bar');
-//     let trigger2Class = $('.score');       //トリガーとなるクラス
-//         animate2Class = ('about__bottom__skills__languages__language__description__graph__bar');
+$(function(){
+  $(window).on('scroll',function (){
+    let triggerClass = $('.bar');       //トリガーとなるクラス
+        animateClass = ('about__bottom__skills__container__skill__graph__bar');
+    let trigger2Class = $('.score');       //トリガーとなるクラス
+        animate2Class = ('about__bottom__skills__languages__language__description__graph__bar');
  
-//     $(triggerClass).each(function(){
-//       let scroll       = $(window).scrollTop(), //スクロール位置を取得
-//           triggerTop   = $(this).offset().top,  //指定要素のY座標
-//           windowHeight = $(window).height();    //ウィンドウの高さ
-//       if (scroll > triggerTop - windowHeight){
-//         $(this).addClass(animateClass);
-//       }
-//     });
+    $(triggerClass).each(function(){
+      let scroll       = $(window).scrollTop(), //スクロール位置を取得
+          triggerTop   = $(this).offset().top,  //指定要素のY座標
+          windowHeight = $(window).height();    //ウィンドウの高さ
+      if (scroll > triggerTop - windowHeight){
+        $(this).addClass(animateClass);
+      }
+    });
 
-//     $(trigger2Class).each(function(){
-//       let scroll       = $(window).scrollTop(), //スクロール位置を取得
-//           triggerTop   = $(this).offset().top,  //指定要素のY座標
-//           windowHeight = $(window).height();    //ウィンドウの高さ
-//       if (scroll > triggerTop - windowHeight){
-//         $(this).addClass(animate2Class);
-//       }
-//     });
+    $(trigger2Class).each(function(){
+      let scroll       = $(window).scrollTop(), //スクロール位置を取得
+          triggerTop   = $(this).offset().top,  //指定要素のY座標
+          windowHeight = $(window).height();    //ウィンドウの高さ
+      if (scroll > triggerTop - windowHeight){
+        $(this).addClass(animate2Class);
+      }
+    });
 
-//   });
-//  });
+  });
+ });
