8988ced456c4e9f64f148f49b4e3681e9a559cbd
mayukuno <kunomayu1006@gmail.com>
2020-12-20 11:37:50 +0900

    updated

--- app/models/vlog.rb
+++ app/models/vlog.rb
@@ -1,9 +1,11 @@
 class Vlog < ApplicationRecord
-    validates :title, :content, presence: true
+  validates :title, :content, presence: true
   mount_uploader :image, ImageUploader
   
+  # https://pgmg-rails.com/blogs/21
+  # 下書き機能
   has_many :vlog_categories, dependent: :destroy
   has_many :categories,  through:  :vlog_categories, dependent: :destroy
   enum status: { draft: 0, published: 1 }
-  
+
 end
